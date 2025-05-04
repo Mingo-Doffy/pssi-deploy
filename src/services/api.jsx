16a85ccee6428f773ededab32e2f7f27ccdc835b
DOFLAMINGO <doffymingo519@gmail.com>
2025-05-04 17:05:18 +0000

    pssi-deploy v1.0.1Tc3 - Mise a jour

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://backend.railway.internal/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://pssi-deploy.railway.internal/api',
   headers: {
     'Content-Type': 'application/json'
   }
